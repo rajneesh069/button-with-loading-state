7db3da2bcbb8074ee0ffd1178903fc92a0d6640b
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-05-19 11:01:31 +0530

    Add .vercelignore to ignore the server folder while deploying

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080"; //for local development
+export const BASE_URL = "https://button-with-loading-state.onrender.com";
